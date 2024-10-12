2754088e62090bcbe8e1ed7cff9a42b198fe5173
itstaheri <itstaheri1@gmail.com>
2024-10-12 22:24:36 +0330

    add project to repository github

---
+++ Shop.Application/Services/IUserService.cs
@@ -0,0 +1,63 @@
+﻿using Shop.Domain.Dtos;
+using Shop.Domain.Dtos.User;
+using Shop.Domain.Repositories.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Common.Cryptography;
+using Shop.Domain.Repositories;
+using Shop.Domain.Enums;
+using Shop.Domain.Entities.User;
+
+namespace Shop.Application.Services
+{
+    public interface IUserService
+    {
+        public OperationResult<UserInfoDto> Login(LoginDto login);
+        public Task<OperationResult> SignupAsync(CreateUserDto createUser,CancellationToken cancellationToken);
+    }
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IGenericRepository<RoleModel> _role;
+        public UserService(IUserRepository userRepository, IGenericRepository<RoleModel> role)
+        {
+            _userRepository = userRepository;
+            _role = role;
+        }
+
+        public OperationResult<UserInfoDto> Login(LoginDto login)
+        {
+            try
+            {
+                var user = _userRepository.CheckUserValid(login.Username, login.Password);
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message,ex.InnerException);
+            }
+        }
+
+        public async Task<OperationResult> SignupAsync(CreateUserDto createUser, CancellationToken cancellationToken)
+        {
+            try
+            {
+                createUser.Password = createUser.Password.ToSha256();
+                var signup = await _userRepository.CreateUserAsync(createUser, cancellationToken);
+
+                return signup;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message,ex.InnerException);
+            }
+        }
+    }
+}
---
+++ Shop.Infrastructure/InfrastructureResolver.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Domain.Repositories;
+using Shop.Infrastructure.Database.SqlServer.Efcore;
+using Shop.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Infrastructure
+{
+    public static class InfrastructureResolver
+    {
+        public static void ResolveInfrastructure(this IServiceCollection services,string connectionString)
+        {
+            services.AddDbContext<ShopDbContext>(x=>x.UseSqlServer(connectionString),ServiceLifetime.Scoped);
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
+    }
+}
