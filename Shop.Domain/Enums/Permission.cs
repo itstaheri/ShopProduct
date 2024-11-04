826997c419501bce0922638f056d025d4dd32c64
mohammad norouzi <mohammadnorouzi7999@gmail.com>
2024-11-05 00:09:44 +0330

    set Permission Id in Enum

--- Shop.Domain/Enums/Permission.cs
+++ Shop.Domain/Enums/Permission.cs
@@ -10,31 +10,29 @@ namespace Shop.Domain.Enums
     {
         All = 1,
 
-        ReadProduct,
-        WriteProduct,
-        EditProduct,
-        DeleteProduct,
+        ReadProduct = 2,
+        WriteProduct = 3,
+        EditProduct = 4,
+        DeleteProduct = 5,
 
-        ReadInventory,
-        WriteInventory,
-        EditInventory,
-        DeleteInventory,
+        ReadInventory = 6,
+        WriteInventory = 7,
+        EditInventory = 8,
+        DeleteInventory = 9,
 
-        ReadCategory,
-        WriteCategory,
-        EditCategory,
-        DeleteCategory,
+        ReadCategory = 10,
+        WriteCategory = 11,
+        EditCategory = 12,
+        DeleteCategory = 13,
 
-        ReadPropery,
-        WritePropery,
-        EditPropery,
-        DeletePropery,
+        ReadPropery = 14,
+        WritePropery = 15,
+        EditPropery = 16,
+        DeletePropery = 17,
 
-        ReadUser,
-        WriteUser,
-        EditUser,
-        DeleteUser,
-
-       
+        ReadUser = 18,
+        WriteUser = 19,
+        EditUser = 20,
+        DeleteUser = 21,
     }
 }
